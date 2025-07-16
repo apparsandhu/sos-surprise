13c1cf9158ab5f393cf50571a7e5284fb9faabc9
apparsandhu <apparjotkaur@gmail.com>
2025-07-16 22:42:03 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function retry() {
+  setTimeout(() => {
+    window.location.href = "emergency.html";
+  }, 1000);
+}
+
+function callApar() {
+  setTimeout(() => {
+    window.location.href = "bday.html";
+  }, 1000);
+}
